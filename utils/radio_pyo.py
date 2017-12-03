7987c85c553ed513aa683470c5c297304c13d2c6
akj <akjmicro@gmail.com>
2017-12-03 17:28:08 -0600

    much cleanup and functional change; new randomization mechanism.

--- utils/radio_pyo.py
+++ utils/radio_pyo.py
@@ -6,23 +6,57 @@ import datetime
 import glob
 import sys
 import shutil
+import tokenize
+import io
+
+from pyo import sndinfo
+
+RADIOPYO_PATH = '/xxxx/'
+PLAYLIST_NO_REPEAT_LEN = 15
+
+
+def read_queue_history():
+    try:
+        with open(os.path.join(RADIOPYO_PATH, 'queue_history')) as queue_hist:
+            last_n = queue_hist.readlines()[-PLAYLIST_NO_REPEAT_LEN:]
+    except IOError:
+        open(os.path.join(RADIOPYO_PATH, 'queue_history'), 'w').close()
+        with open(os.path.join(RADIOPYO_PATH, 'queue_history')) as queue_hist:
+            last_n = queue_hist.readlines()[-PLAYLIST_NO_REPEAT_LEN:]
+    return last_n
+
+
+def write_queue_history(songname):
+    last_n = read_queue_history()
+    last_n.append('{0}\n'.format(songname))
+    last_n = last_n[-PLAYLIST_NO_REPEAT_LEN:]
+    with open(os.path.join(RADIOPYO_PATH, 'queue_history'), 'w') as queue_hist:
+        queue_hist.write(''.join(last_n))
+    return None
+
+
+def get_random_song(path):
+    all_songs = [i for i in glob.glob(path + '*.ogg')]
+    song = random.choice(all_songs)
+    last_n = read_queue_history()
+    # do not pick file if it's marked for deletion or locked:
+    while True:
+        stamp_files = [
+            i for i in glob.glob(os.path.splitext(song)[0] + '*.stamp')]
+        locked_files = [
+            i for i in glob.glob(os.path.splitext(song)[0] + '.lock')]
+        if (song in last_n) or stamp_files or locked_files:
+            song = random.choice(all_songs)
+        else:
+            break
+    return song
 
 
 def select_song(path=None):
-    songs_list = [i for i in glob.glob(path + '*.ogg')]
-    song = random.choice(songs_list)
-    # Must ask Tiago: why the recursive calls here? Is this a waiting
-    # mechanism?
-    # do not pick file if it's marked for deletion
-    stamp_files = [i for i in glob.glob(os.path.splitext(song)[0] + '*.stamp')]
-    if len(stamp_files) > 0:
-        return select_song(path)
-    # do not pick a locked file
-    if os.path.isfile(song + '.lock'):
-        return select_song(path)
+    song = get_random_song(path)
     # ices2 needs this
-    print song
-    from pyo import sndinfo
+    print(song)
+    hist_stat = write_history(song)
     song_duration = sndinfo(song)[1]
     now = datetime.datetime.now()
     # tag for update at now + song duration
@@ -47,11 +81,11 @@ def update_song(script_file, ogg_file_tmp):
         os.path.splitext(script_file)[0] + '*.stamp')]
     # remove other eventual stamps for the same file
     for f in stamp_files:
-        print 'Removing {0}'.format(f)
+        print('Removing {0}'.format(f))
         os.remove(f)
     # lock it to not be played during rendering
     open(ogg_file + '.lock', 'a').close()
-    render_cmd = script_file + " " + ogg_file_tmp
+    render_cmd = '{0} {1}'.format(script_file, ogg_file_tmp)
     os.system(render_cmd)
     song_info = get_song_info(script_file)
     # TODO: use a proper python library for this
@@ -79,19 +113,13 @@ def get_song_info(script_file):
         if l.split('=')[0].strip() == 'DURATION':
             song_info_dict['DURATION'] = clean_string(os.path.splitext(
                 nocomment(l.split('=')[1]))[0])  # deal with seconds in float format
-    # print song_info_dict['DURATION']
     return song_info_dict
-
-
-import tokenize
-import io
 
 
 def nocomment(string):
     result = []
     g = tokenize.generate_tokens(io.BytesIO(string).readline)
     for toknum, tokval, _, _, _ in g:
-        # print(toknum,tokval)
         if toknum != tokenize.COMMENT:
             result.append((toknum, tokval))
     return tokenize.untokenize(result)
@@ -121,21 +149,22 @@ def update_all_songs(path=None, tmp_filename=None):
     for name in glob.glob(path + '*.py'):
         update_song(name, tmp_filename)
 
-### Main program ###
 
+def main():
+    cmdargs = sys.argv
+    if len(cmdargs) == 1:
+        select_song(RADIOPYO_PATH)
+    else:
+        import string
+        tmp_filename = ''.join([random.choice(
+            string.ascii_letters + string.digits) for n in xrange(10)]) \
+            + '.ogg'
+        if cmdargs[1] == 'update':
+            update_songs(RADIOPYO_PATH, tmp_filename)
+        elif cmdargs[1] == 'update_all':
+            update_all_songs(RADIOPYO_PATH, tmp_filename)
+        elif cmdargs[1] == 'update_song':
+            update_song(cmdargs[2])
 
-RADIOPYO_PATH = "/xxxx/"
-cmdargs = sys.argv
-
-if len(cmdargs) == 1:
-    select_song(RADIOPYO_PATH)
-else:
-    import string
-    tmp_filename = ''.join([random.choice(
-        string.ascii_letters + string.digits) for n in xrange(10)]) + '.ogg'
-    if cmdargs[1] == 'update':
-        update_songs(RADIOPYO_PATH, tmp_filename)  # path + output filename
-    elif cmdargs[1] == 'update_all':
-        update_all_songs(RADIOPYO_PATH, tmp_filename)
-    elif cmdargs[1] == 'update_song':
-        update_song(cmdargs[2])
+if __name__ == '__main__':
+    main()
