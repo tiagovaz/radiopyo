f53779d060550dee833b6f0bd3281bbfb15b0823
Rose Hirigoyen <rosehirigoyen@gmail.com>
2021-07-29 14:18:58 -0400

    added docstrings and cleaned a bit webpage

--- website/js/utils.js
+++ website/js/utils.js
@@ -6,6 +6,9 @@ var songQueue = [];
 
 var PLAYLIST_NO_REPEAT_LEN;
 
+/**
+ * Callback to handle play button click
+ */
 const onPlayBtnClick = function() {
     if(!isPlaying) {
         playAudio()
@@ -14,34 +17,64 @@ const onPlayBtnClick = function() {
     }
 }
 
+
+/**
+ * Callback to handle end of audio file
+ */
+ const onSongEnd = function() {
+    isPlaying = false;
+    chooseNextSong();
+    playAudio();
+}
+
+
+/**
+ * Choose an random element from an array
+ * @param  {Object} array The array to choose from
+ * @return {Object}       The chosen element from the array
+ */
 const randomChoice = function(array) {
     // source: https://stackoverflow.com/a/4550514
     return array[Math.floor(Math.random() * array.length)];
 }
 
+
+/**
+ * Removes element in array that are in an other array
+ * @param {Object}  toKeep   Array from which to remove elements
+ * @param {Object}  toRemove Array containing elements to remove
+ * @return {Object}          The array containing all elements in toKeep that are not also in toRemove
+ */
 const difference = function(toKeep, toRemove) {
     // source: https://stackoverflow.com/a/33034768
     return toKeep.filter(x => !toRemove.includes(x));
 }
 
+
+/**
+ * Start a song
+ */
 const playAudio = function() {
     isPlaying = true;
     playBtn.classList.add('playing');
     audio.play();
 }
 
+
+/**
+ * Pause a song
+ */
 const pauseAudio = function() {
     isPlaying = false;
     playBtn.classList.remove('playing');
     audio.pause();
 }
 
-const onSongEnd = function() {
-    isPlaying = false;
-    chooseNextSong();
-    playAudio();
-}
 
+/**
+ * Load a song in the audio player but don't start it
+ * @param {Object}  song   Metadata of the song to load
+ */
 const loadSong = function(song) {
     audioSource.src = song.PATH;
     audio.load();
@@ -51,17 +84,31 @@ const loadSong = function(song) {
     showSongMetadata(song);
 }
 
+
+/**
+ * Get a random song from all available songs that was not played in the last N songs
+ * @return {Object} A song metadata
+ */
 const getRandomSong = function() {
     possibleSongs = difference(songsMetadata, songQueue);
     song = randomChoice(possibleSongs);
     return song;
 }
 
+
+/**
+ * Get a random song and load it in the audio player
+ */
 const chooseNextSong = function() {
     nextSong = getRandomSong();
     loadSong(nextSong);
 }
 
+
+/**
+ * Change the UI to show what song is playing
+ * @param {Object}  song   Metadata of a song
+ */
 const showSongMetadata = function(song) {
     authorText = document.getElementById("author");
     authorText.textContent = song.ARTIST;
